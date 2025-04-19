ec32b810e6acb56f3623a973bbb5b2ffcf3861f0
Grey3016 <141076190+Grey3016@users.noreply.github.com>
2025-04-19 15:43:43 +0100

    Update Manually Installed Version - Update.bat

--- Manually Installed Version - Update.bat
+++ Manually Installed Version - Update.bat
@@ -1,12 +1,12 @@
 @echo off
 
-call environment.bat
+@REM call environment.bat
 
-git -C "%~dp0webui" pull 2>NUL
+git -C "%~dp0FramePack" pull 2>NUL
 if %ERRORLEVEL% == 0 goto :done
 
-git -C "%~dp0webui" reset --hard
-git -C "%~dp0webui" pull
+git -C "%~dp0FramePack" reset --hard
+git -C "%~dp0FramePack" pull
 
 :done
 pause
