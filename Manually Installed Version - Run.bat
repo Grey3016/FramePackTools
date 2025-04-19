05951a672f2d05cbfba41e0defe73de3ee74cc14
Grey3016 <141076190+Grey3016@users.noreply.github.com>
2025-04-19 15:43:12 +0100

    Update Manually Installed Version - Run.bat

--- Manually Installed Version - Run.bat
+++ Manually Installed Version - Run.bat
@@ -1,10 +1,11 @@
 @echo off
 
-call environment.bat
+@REM call environment.bat
 
-cd %~dp0webui
+cd FramePack
 
-"%DIR%\python\python.exe" demo_gradio.py --server 127.0.0.1 --inbrowser
+call venv\Scripts\Activate.bat
+.\venv\Scripts\python.exe -s demo_gradio.py --server 127.0.0.1 --inbrowser
 
-:done
+
 pause
